b36063e9767eca5467823757f8e6e3ea4593a485
kathypapabi <kathpapabi@gmail.com>
2023-05-02 16:28:09 -0700

    task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_rev - imprime an reversa
+ * print_rev - imprime en reversa
  * @s: string
  * return: 0
  */
@@ -8,6 +8,7 @@ void print_rev(char *s)
 {
 	int longi = 0;
 	int o;
+
 	while (*s != '\0')
 	{
 		longi++;
@@ -19,5 +20,6 @@ void print_rev(char *s)
 		_putchar(*s);
 		s--;
 	}
+
 	_putchar('\n');
 }
